a52a4a10b2d41b602a4e22e33df11303e9b20bc5
谢昆 <xiekun@sunlands.com>
2020-04-18 17:58:13 +0800

    update .gitignore

--- baike_sp/html_downloader.py
+++ baike_sp/html_downloader.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#@Time  : 2020/4/18 15:18
-#@Author: xk
-#@File  : html_downloader.py
+# @Time  : 2020/4/18 15:18
+# @Author: xk
+# @File  : html_downloader.py
+import gzip
+import urllib.request
+from io import BytesIO
+
+import requests
+from bs4 import BeautifulSoup
+
+
 class HtmlDownloader(object):
-    def download(self, new_url):
-        pass
+
+    # 下载html
+    def download(self, url):
+        if url is None:
+            return
+
+        response = requests.get(url)
+
+        response = urllib.request.urlopen(url)
+        code = response.code
+
+        if code != 200:
+            return None
+
+        return response.read().decode('utf-8')
+
+
+if __name__ == '__main__':
+    print("入口")
+    dw = HtmlDownloader()
+    content = dw.download("http://www.baidu.com")
+    print('打印：', content, '\n')  # 打印
+
--- baike_sp/url_manager.py
+++ baike_sp/url_manager.py
@@ -6,15 +6,32 @@
 
 
 class UrlManager(object):
-    def add_new_url(self, root_url):
-        pass
 
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
+
+    # 向管理器中添加url
+    def add_new_url(self, url):
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
+
+    # 向管理器中添加批量的url
+    def add_new_urls(self, new_urls):
+        if new_urls is None or len(new_urls) == 0:
+            return
+        for url in new_urls:
+            self.add_new_url(url)
+
+    # 判断管理器中是否有新的url
     def has_new_url(self):
-        pass
+        return len(self.new_urls) != 0
 
+    # 获取管理器中最新的url
     def get_new_url(self):
-        pass
-
-    def add_new_urls(self, new_url):
-        pass
-
+        # pop() 获取最上层的url，并将该url移除
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
